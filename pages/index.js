916f040e1279692d75c775f8aacf90d96f93d989
patmood <patrick.n.moody@gmail.com>
2015-11-30 23:58:42 -0800

    render 6 pack photo

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,13 @@
 import React, { Component } from 'react'
 import AvatarEditor from 'react-avatar-editor'
 
+const sizes = {
+  picHeight: 300,
+  picWidth: 300,
+  border: 30,
+}
+
+
 export default class extends Component {
   constructor(props) {
     super(props)
@@ -20,34 +27,56 @@ export default class extends Component {
     return (
       <div>
         <h1>Home Page</h1>
-        <p>Coming soon.</p>
+        <p>Standard 135 film & print size in US, Canada, Australia and India. Called "10 × 15 cm" worldwide.</p>
         <AvatarEditor
           image="http://i.imgur.com/y7yZHAF.jpg"
-          width={250}
-          height={250}
-          border={50}
+          width={sizes.picWidth}
+          height={sizes.picHeight}
+          border={sizes.border}
           ref='editor'
           scale={this.state.scale} />
-        <input
-          type="range"
-          min={0.1}
-          max={5}
-          step={0.1}
-          defaultValue={parseFloat(this.state.scale)}
-          onChange={this.changeScale.bind(this)} />
-        <button onClick={this.processImage.bind(this)}>Process</button>
-        <img src={this.state.processedImage} />
+        <div>
+          <label>Zoom:</label>
+          <input
+            type="range"
+            min={0.1}
+            max={5}
+            step={0.1}
+            defaultValue={this.state.scale}
+            onChange={this.changeScale.bind(this)} />
+          <button onClick={this.processImage.bind(this)}>Process</button>
+        </div>
+        <div>
+          <canvas
+            ref='canvas'
+            width={900}
+            height={600}></canvas>
+        </div>
       </div>
     )
   }
 
   changeScale (e) {
-    this.setState({ scale: e.target.value })
+    this.setState({ scale: parseFloat(e.target.value) })
   }
 
   processImage () {
     const dataUrl = this.refs.editor.getImage()
+    this.drawCanvas(dataUrl)
     this.setState({ processedImage: dataUrl })
   }
 
+  drawCanvas (dataUrl) {
+    const canvas = this.refs.canvas
+    if (!canvas) return console.log('Canvas not supported')
+
+    const ctx = canvas.getContext('2d')
+    const img = new Image(dataUrl)
+    img.src = dataUrl
+    for (let x = 0; x <= 3; x++) {
+      for (let y = 0; y <= 2; y++) {
+        ctx.drawImage(img, x * canvas.width / 3, y * canvas.height / 2)
+      }
+    }
+  }
 }
