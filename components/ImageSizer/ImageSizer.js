4e3788645ddd2b8a2fd0982e1153b7bd946df388
patmood <patrick.n.moody@gmail.com>
2015-12-03 23:42:20 -0800

    image sizer component

---
+++ components/ImageSizer/ImageSizer.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+import './ImageSizer.scss'
+
+import AvatarEditor from 'react-avatar-editor'
+import RangeInput from '../RangeInput'
+
+import { partial } from 'lodash'
+
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      scale: 1,
+      sourceImage: 'http://i.imgur.com/y7yZHAF.jpg',
+    }
+    this.changeScale = this.changeScale.bind(this)
+  }
+
+  render () {
+    const { sizes } = this.props
+    return <div>
+      <div style={{ position: 'relative', width: sizes.picWidth, margin: 'auto' }}>
+        <AvatarEditor
+          image={this.state.sourceImage}
+          width={sizes.picWidth}
+          height={sizes.picHeight}
+          border={sizes.border}
+          ref='editor'
+          scale={this.state.scale} />
+        <canvas
+          width={sizes.picWidth}
+          height={sizes.picHeight}
+          ref='circleOverlay'
+          style={{ pointerEvents: 'none', position: 'absolute', top: 0, left: 0 }}>
+        </canvas>
+        <i
+          onClick={partial(this.changeScale, -0.1)}
+          className='fa fa-minus-circle zoom-overlay-button'
+          style={{top: 30, left: 30}}></i>
+        <i
+          onClick={partial(this.changeScale, 0.1)}
+          className='fa fa-plus-circle zoom-overlay-button'
+          style={{top: 30, right: 30}}></i>
+      </div>
+      <div className='zoom-controls'>
+        <div className='zoom-out' onClick={partial(this.changeScale, -0.1)}>
+          <i className='fa fa-minus'></i>
+        </div>
+        <div className='slider'>
+          <RangeInput
+            min={0.1}
+            max={5}
+            step={0.1}
+            defaultValue={this.state.scale}
+            value={this.state.scale}
+            onChange={this.handleRangeChange.bind(this)} />
+        </div>
+        <div className='zoom-in' onClick={partial(this.changeScale, 0.1)}>
+          <i className='fa fa-plus'></i>
+        </div>
+      </div>
+      <div>
+        <button onClick={this.handleProcessImage.bind(this)} className='btn btn-green' style={{ width: 200 }}>
+          { this.props.isProcessing
+            ? <i className='fa fa-cog fa-spin'></i>
+            : 'Process' }
+        </button>
+      </div>
+    </div>
+  }
+
+  componentDidUpdate () {
+    this.drawCircleOverlay()
+  }
+
+  handleProcessImage () {
+    const dataUrl = this.refs.editor.getImage()
+    this.props.processImage(dataUrl)
+  }
+
+  handleRangeChange (e) {
+    this.setState({ scale: parseFloat(e.target.value) })
+  }
+
+  changeScale (delta) {
+    const scale = this.state.scale + delta
+    this.setState({ scale })
+  }
+
+  drawCircleOverlay () {
+    const canvas = this.refs.circleOverlay
+    const context = canvas.getContext('2d')
+    var centerX = canvas.width / 2
+    var centerY = canvas.height * 0.4
+    var radius = canvas.width * 0.2
+
+    context.beginPath()
+    context.strokeStyle = '#003300'
+    context.lineWidth = 5
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
+    context.stroke()
+    context.closePath()
+  }
+
+  static getImage () {
+    return this.refs.editor.getImage()
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,17 @@
  */
 
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import './index.scss'
 
-import AvatarEditor from 'react-avatar-editor'
 import PhotoStandard from '../components/PhotoStandard'
-import RangeInput from '../components/RangeInput'
-import { partial } from 'lodash'
+import ImageSizer from '../components/ImageSizer'
 
 const sizes = {
   picHeight: 600,
   picWidth: 600,
   border: 0,
 }
-
 
 export default class extends Component {
   constructor(props) {
@@ -28,11 +26,6 @@ export default class extends Component {
       sourceImage: 'http://i.imgur.com/y7yZHAF.jpg',
       isProcessing: false,
     }
-    this.changeScale = this.changeScale.bind(this)
-  }
-
-  componentDidMount () {
-    this.drawCircleOverlay()
   }
 
   render () {
@@ -49,75 +42,21 @@ export default class extends Component {
           />
         </div>
         <p>Zoom and crop your photo below:</p>
-        <div style={{ position: 'relative', width: sizes.picWidth, margin: 'auto' }}>
-          <AvatarEditor
-            image={this.state.sourceImage}
-            width={sizes.picWidth}
-            height={sizes.picHeight}
-            border={sizes.border}
-            ref='editor'
-            scale={this.state.scale} />
-          <canvas
-            width={sizes.picWidth}
-            height={sizes.picHeight}
-            ref='circleOverlay'
-            style={{ pointerEvents: 'none', position: 'absolute', top: 0, left: 0 }}>
-          </canvas>
-          <i
-            onClick={partial(this.changeScale, -0.1)}
-            className='fa fa-minus-circle zoom-overlay-button'
-            style={{top: 30, left: 30}}></i>
-          <i
-            onClick={partial(this.changeScale, 0.1)}
-            className='fa fa-plus-circle zoom-overlay-button'
-            style={{top: 30, right: 30}}></i>
-        </div>
-        <div className='zoom-controls'>
-          <div className='zoom-out' onClick={partial(this.changeScale, -0.1)}>
-            <i className='fa fa-minus'></i>
-          </div>
-          <div className='slider'>
-            <RangeInput
-              min={0.1}
-              max={5}
-              step={0.1}
-              defaultValue={this.state.scale}
-              value={this.state.scale}
-              onChange={this.handleRangeChange.bind(this)} />
-          </div>
-          <div className='zoom-in' onClick={partial(this.changeScale, 0.1)}>
-            <i className='fa fa-plus'></i>
-          </div>
-        </div>
-        <div>
-          <button onClick={this.processImage.bind(this)} className='btn btn-green' style={{ width: 200 }}>
-            { this.state.isProcessing
-              ? <i className='fa fa-cog fa-spin'></i>
-              : 'Process' }
-          </button>
-        </div>
-        <PhotoStandard image={this.state.processedImage}/>
+        <ImageSizer
+          sizes={sizes}
+          ref='imageSizer'
+          isProcessing={this.state.isProcessing}
+          processImage={this.processImage.bind(this)} />
+        <PhotoStandard image={this.state.processedImage} />
       </div>
     )
   }
 
-  componentDidUpdate () {
-    this.drawCircleOverlay()
-  }
-
-  handleRangeChange (e) {
-    this.setState({ scale: parseFloat(e.target.value) })
-  }
-
-  changeScale (delta) {
-    const scale = this.state.scale + delta
-    this.setState({ scale })
-  }
-
-  processImage () {
+  processImage (dataUrl) {
     this.setState({ isProcessing: true })
-    const dataUrl = this.refs.editor.getImage()
     this.drawCanvas(dataUrl)
+    const node = ReactDOM.findDOMNode(this.refs.imageSizer)
+    window.scrollTo(0, node.scrollHeight + node.offsetHeight)
   }
 
   drawCanvas (dataUrl) {
@@ -158,21 +97,6 @@ export default class extends Component {
     }
 
     img.src = dataUrl
-  }
-
-  drawCircleOverlay () {
-    const canvas = this.refs.circleOverlay
-    const context = canvas.getContext('2d')
-    var centerX = canvas.width / 2
-    var centerY = canvas.height * 0.4
-    var radius = canvas.width * 0.2
-
-    context.beginPath()
-    context.strokeStyle = '#003300'
-    context.lineWidth = 5
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false)
-    context.stroke()
-    context.closePath()
   }
 
   getSourceImage (e) {
